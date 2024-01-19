655d18b09bc289ed34566bb5494d28bb57d1efca
coderdavidgs <davidgbrdsantos@gmail.com>
2024-01-18 21:26:22 -0300

    create the query client by id

--- resolvers/Query/Client.js
+++ resolvers/Query/Client.js
@@ -12,6 +12,25 @@ const clientQuery = {
             'phone_number as phoneNumber'
         );
     },
+
+    async client(_, { input }) {
+        const { id } = input;
+        const verifyIdExists = await conn('clients').where({ id }).first();
+
+        if(verifyIdExists) {
+            return await conn('clients').select(
+                'id',
+                'name',
+                'last_name as lastName',
+                'phone_number as phoneNumber',
+                'email',
+                'job',
+                'active'
+            ).where({ id }).first();
+        }
+
+        return undefined;
+    },
 };
 
 export default clientQuery;
