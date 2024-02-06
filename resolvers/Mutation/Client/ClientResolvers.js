0a17aacabf6611b45e9a8e070a3a9ca66aaa9f74
coderdavidgs <davidgbrdsantos@gmail.com>
2024-02-05 21:46:09 -0300

    create the update client function

--- resolvers/Mutation/Client/ClientResolvers.js
+++ resolvers/Mutation/Client/ClientResolvers.js
@@ -24,6 +24,33 @@ const clientResolvers = {
 
         return undefined;
     },
+
+    async updateClient(_, { input }) {
+        const { id, job, phoneNumber } = input;
+        const verifyIdExists = await conn('clients').where({ id }).first();
+
+        if(verifyIdExists) {
+            const data = {}
+            if(job) data.job = job;
+            if(phoneNumber) data.phone_number = phoneNumber;
+
+            await conn('clients').update(data).where({ id });
+            
+            const response = await conn('clients').select(
+                'id',
+                'name',
+                'last_name as lastName',
+                'phone_number as phoneNumber',
+                'email',
+                'job',
+                'active'
+            ).where({ id }).first();
+
+            return id ? { ...response } : undefined;
+        }
+
+        return undefined;
+    },
 };
 
 export default clientResolvers;
